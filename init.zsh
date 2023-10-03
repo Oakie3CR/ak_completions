92683e80e45985efff6aa8f28434beb15e749c10
Christian Rigdon <christian.rigdon@octanner.com>
2023-10-03 17:37:25 -0600

    Switch to arm path

--- init.zsh
+++ init.zsh
@@ -3,7 +3,7 @@ _aka_yargs_completions()
   local reply
   local si=$IFS
   IFS=$'
-' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" /usr/local/bin/ak --get-yargs-completions "${words[@]}"))
+' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" /opt/homebrew/bin/ak --get-yargs-completions "${words[@]}"))
   IFS=$si
   _describe 'values' reply
 }
